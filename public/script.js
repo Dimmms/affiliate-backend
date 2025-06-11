0932ad579b96f7b7e003c33a047edc8f9736869b
Dimmmms <dimmms@gmail.com>
2025-06-11 21:12:11 +0700

    fix: add dotenv to dependencies

--- public/script.js
+++ public/script.js
@@ -1,1 +1,8 @@
-// script.js sudah dibersihkan dari fitur slider otomatis
+
+fetch("https://web-production-bf13b.up.railway.app/create-transaction", {
+  method: "POST",
+  headers: { "Content-Type": "application/json" },
+  body: JSON.stringify({
+    name, email, phone, product_description, gross_amount
+  })
+})
