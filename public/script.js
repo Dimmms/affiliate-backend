8b4644ec4ef17d43bc4cad1bdeddbb264dada211
Dimmmms <dimmms@gmail.com>
2025-06-12 22:14:34 +0700

    init backend

--- public/script.js
+++
@@ -1,9 +0,0 @@
-fetch("http://localhost:3000/create-transaction", {
-  method: "POST",
-  headers: { "Content-Type": "application/json" },
-  body: JSON.stringify({
-    nama,
-    email,
-    whatsapp
-  })
-})
